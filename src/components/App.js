e139e5e8af77d868c0491f5373abac58eaf786f1
Eric Kim <erickim195@gmail.com>
2020-05-22 14:33:22 -0700

    new website build

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+    return (
+        <div>This is my website</div>
+    );
+};
+
+render(<App />, document.getElementById("root"));
