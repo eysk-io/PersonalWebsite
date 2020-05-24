02787ec837aef104a6627720de29be0ceec59da2
Eric Kim <erickim195@gmail.com>
2020-05-23 17:49:52 -0700

    added hover to appropriate elements

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Todo:
 
-Make Logo/my name scroll to top of page
-Make links hover
 Adjust how each project looks
 Re-factor to make nicer
--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import styled from 'styled-components';
 import projects from '../assets/projects';
 import { Header, Footer } from './global/index';
 import Layout from './layout/Layout';
 import Jumbotron from './title/Title';
 import ProjectPanels from './project-panels/ProjectPanels';
 
+const AppStyle = styled.div`
+    a :hover {
+        transform: translateY(-3px);
+    }
+`;
+
 const App = () => {
     return (
-        <div className="root">
-            <Header />
-            <Jumbotron />
-            <Layout>
-                <ProjectPanels projects={projects} />
-            </Layout>
-            <Footer />
-        </div >
+        <AppStyle>
+            <div className="root">
+                <Header />
+                <Jumbotron />
+                <Layout>
+                    <ProjectPanels projects={projects} />
+                </Layout>
+                <Footer />
+            </div >
+        </AppStyle>
     );
 };
 
