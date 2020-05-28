2c9c06e4e5924864ef8682a8960b45a96a55a706
Eric Kim <erickim195@gmail.com>
2020-05-28 00:46:46 -0700

    finished footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Styles = styled.div`
-    .footer {
-        background: linear-gradient(to right, #292b2c, #000000);
-        height: 60px;
-    }
-`;
+import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faGitlab } from '@fortawesome/free-brands-svg-icons';
+import { FooterStyles } from './global-styles/global.styles';
 
 const Footer = () => {
     return (
-        <Styles>
+        <FooterStyles>
             <div className="footer">
+                <div className="footer-content">
+                    <div className="footer-text">
+                        Find me at other places too! -
+                    </div>
+                    <div className="footer-icons">
+                        <a href="https://linkedin.com/in/eykim195/" target="_blank">
+                            <FontAwesomeIcon href="" className="social-media" size={"3x"} id="linkedin" icon={faLinkedin} />
+                        </a>
+                        <a href="https://github.com/eyskim" target="_blank">
+                            <FontAwesomeIcon className="social-media" size={"3x"} id="github" icon={faGithub} />
+                        </a>
+                        <a href="https://gitlab.com/eyskim" target="_blank">
+                            <FontAwesomeIcon href="" className="social-media" size={"3x"} id="gitlab" icon={faGitlab} />
+                        </a>
+                    </div>
+                </div>
             </div>
-        </Styles>
+        </FooterStyles>
     );
 }
 
--- src/components/global/global-styles/global.styles.js
+++ src/components/global/global-styles/global.styles.js
@@ -34,4 +34,44 @@ const HeaderStyles = styled.div`
     }
 `;
 
-export { HeaderStyles };
+const FooterStyles = styled.div`
+    .footer {
+        background: rgb(44, 45, 45);
+        min-height: 100px;
+        font-family: 'Roboto', sans-serif;
+        color: #fff;
+        font-size: 12px;
+        line-height: 30px;
+        font-weight: 400;
+        letter-spacing: 3px;
+        text-transform: none;
+        position: relative;
+        padding: 15px;
+    }
+
+    .footer-content {
+        max-width: 800px;
+        margin: auto;
+        text-align: center;
+    }
+
+    .footer-text {
+        margin-top: 15px;
+    }
+    
+    .social-media {
+        margin: 15px;
+    }
+
+    #linkedin {
+        color: #0e76a8;
+    }
+    #github {
+        color: #6e5494;
+    }
+    #gitlab {
+        color: #fa7035;
+    }
+`;
+
+export { HeaderStyles, FooterStyles };
