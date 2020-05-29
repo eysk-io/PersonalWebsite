f097373784fd1c1fa4c390951c1c9c8eb0d1f829
Eric Kim <erickim195@gmail.com>
2020-05-28 17:12:30 -0700

    made PortfolioTitle responsive to window size

--- src/components/portfolio-title/PortfolioTitle.styles.js
+++ src/components/portfolio-title/PortfolioTitle.styles.js
@@ -1,16 +1,44 @@
 import styled from 'styled-components';
 
 const PortfolioTitleStyles = styled.div`
-    .portfolio-title {
-        color: rgb(44, 45, 45);
-        font-family: "Roboto", sans-serif;
-        font-size: 40px;
-        line-height: 40px;
-        font-weight: 400;
-        letter-spacing: 12px;
-        text-transform: none;
-        margin: auto;
-        margin-bottom: 100px;
+    @media screen and (min-width: 751px) {
+        .portfolio-title {
+            color: rgb(44, 45, 45);
+            font-family: "Roboto", sans-serif;
+            font-size: 50px;
+            line-height: 40px;
+            font-weight: 400;
+            letter-spacing: 12px;
+            text-transform: none;
+            margin: auto;
+            margin-bottom: 100px;
+        }
+    }
+    @media screen and (min-width: 651px) {
+        .portfolio-title {
+            color: rgb(44, 45, 45);
+            font-family: "Roboto", sans-serif;
+            font-size: 30px;
+            line-height: 40px;
+            font-weight: 400;
+            letter-spacing: 8px;
+            text-transform: none;
+            margin: auto;
+            margin-bottom: 100px;
+        }
+    }
+    @media screen and (max-width: 650px) {
+        .portfolio-title {
+            color: rgb(44, 45, 45);
+            font-family: "Roboto", sans-serif;
+            font-size: 20px;
+            line-height: 40px;
+            font-weight: 400;
+            letter-spacing: 4px;
+            text-transform: none;
+            margin: auto;
+            margin-bottom: 100px;
+        }
     }
 `;
 
